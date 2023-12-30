6fabec0d8b9c7319f39bf150a24a56c1ac063afb
Kedar Thakkar <kedarthakkar@berkeley.edu>
2023-12-29 17:49:29 -0800

    Fixes script

--- infer.py
+++ infer.py
@@ -10,6 +10,6 @@ def main(model_filepath, example):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run inference with QG model.')
     parser.add_argument('model_filepath', type='str', help='Filepath of the QG model.')
-    parser.add_argument('example', type='str', help='Example to run QG inference on.')
+    parser.add_argument('-e', dest='example', type='str', help='Example to run QG inference on.')
     args = parser.parse_args()
     main(args.model_filepath, args.example)
